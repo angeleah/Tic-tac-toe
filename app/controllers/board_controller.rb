0f962b9e7262cc75e913a1867670b9ab600e3e85
Angeleah Daidone <angeleah@gmail.com>
2012-03-12 14:20:51 -0500

    Fully working ai less tic tac toe with winner or draw detection.:)

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -30,24 +30,24 @@ class BoardController < ApplicationController
   def set_player_markers 
     @player = Player.first
     if @player[:is_first] 
-       @human_player = "X"
+       @human_player = "X" 
        @computer_player = "O"
       else
-       @human_player = "O"
+       @human_player = "O" 
        @computer_player = "X"    
       end
   end
- 
+  
  def human_move
    @board = Board.first
    @player = Player.first
-   square_pressed = params[:square].to_sym    
    set_player_markers
+   square_pressed = params[:square].to_sym    
    @board[square_pressed] = @human_player
    @board.save
-   computer_move 
-   determine_winner
-   @message   
+   determine_winner_or_draw
+   @message
+   computer_move
    render "board/index" 
  end
  
@@ -55,26 +55,25 @@ class BoardController < ApplicationController
    available_computer_moves = []
    @board.attributes.each do |column_name, column_value|
      if column_value.nil?
-       available_computer_moves << column_name     
+       available_computer_moves << column_name  
      end
    end
    set_player_markers
-   @board[available_computer_moves.first] = @computer_player
+   if available_computer_moves.empty? == true
+     determine_winner_or_draw
+   else 
+     @board[available_computer_moves.first] = @computer_player
+   end   
    @board.save
-   determine_winner
+   determine_winner_or_draw
    @message 
  end
   
- def quit_game
-   @player = Player.first.destroy
-   @board = Board.first.destroy
-   render "home/index"
- end 
- 
- def determine_winner
+ def determine_winner_or_draw
    @possible_wins = [[:s0,:s4,:s8], [:s2,:s4,:s6], [:s0,:s1,:s2], [:s3,:s4,:s5], [:s6,:s7,:s8], [:s0,:s3,:s6], [:s1,:s4,:s7], [:s2,:s5,:s8]]
    @board = Board.first
    @detect_wins = []
+   available_moves = []
    @possible_wins.each do |combination| 
      @group = []
      combination.each do |position|     
@@ -86,112 +85,33 @@ class BoardController < ApplicationController
       if value == ["X","X","X"]
        @message = "Player X is the winner!"
       elsif value == ["O","O","O"]
-        @message = "Player O is the winner!" 
-      end      
+        @message = "Player O is the winner!"  
+      elsif  
+         @board.attributes.each do |column_name, column_value|
+           if column_value.nil?
+             available_moves << column_name       
+           end
+         end
+         if available_moves.empty? == true
+           @message = "It's a draw." 
+          end 
+       end        
     end 
-#    detect_draw  
-#     @message = "#{@board.attributes}"  
  end
- 
- 
- 
- # !!!! There is a problem if you win on the last square press!!!!!!  It will not register. Maybe make an
-#  array with all of the :s0, :s2, etc and pop them off when they are pressed or used by the computer
-#  and then when it has 0 if no one won it would be a draw or if you add them in when it has 9 and no winner it will be a draw. 
- # I believe the problem has to do with the automatic ping ponging back and forth between the human_move and computer_move. 
- #Check the error screen. 
- 
-=begin 
-  def detect_draw
-    @board = Board.first
-    detect_draw = [] 
-    @board.attributes.each do |c, v|
-      if v.nil?
-        detect_draw << v     
-      end
-    end  
-    if detect_draw.empty? == true
-      @message = "It is a draw."
-    end 
-#    render "board/index"  
+
+  def quit_game
+    @player = Player.first.destroy
+    @board = Board.first.destroy
+    render "home/index"
   end
-
-
- def win_lose_draw
-  @board = Board.first
-  @what_is_going_on = []
-  @detect_wins.each do |combination|
-    @what_is_going_on << combination
-    @message = "#{@what_is_going_on}"
-#    
-#    elsif 
- #      @board.attributes.each do |column_name, column_value|
-#          if column_value.all !=nil
- #           @message = "It is a draw."
-#    end  
-#  end  
-  render "board/index" 
- end
-end 
-=end 
  
-=begin
- 
-
-        column_value != nil && column_value != "X" && column_value != "O"
-        @message = "It is a draw."          
 
 
  
-# def determine_winner
-#   @board = Board.first
-#    possible_wins.each do |array|
-# array.each do |column_value|
-#       if column_value.all == "X" 
-#         @message = "X is the winner!"
-#        elsif
-#           column_value.all == "O" && value !nil
-#          @message = "O is the winner!" 
-#        elsif
-#          column_value.!nil && column_value !=
-#          @message = "It is a draw."          
-#       end         
-#      end  
-#    end  
-# end
-
- def possible_wins
-   @diagonal_right = [:s0,:s4,:s8] 
-   @diagonal_left = [:s2,:s4,:s6]
-   @horizontal_top = [:s0,:s1,:s2]
-   @horizontal_middle = [:s3,:s4,:s5]
-   @horizontal_bottom = [:s6,:s7,:s8]
-   @vertical_left = [:s0,:s3,:s6]
-   @vertical_middle = [:s1,:s4,:s7]
-   @vertical_right = [:s2,:s5,:s8]
- end  
  
- def determine_winner
-   @board = Board.first
-   possible_wins.each do |column_name, column_value|
-      if column_value == "X" 
-       @message = "X is the winner!"
-      elsif
-         column_value == "O" 
-        @message = "O is the winner!" 
-      elsif
-        column_value != nil && column_value != "X" && column_value != "O"
-        @message = "It is a draw."          
-      end
-   end  
- end
-   
-     @message_loser = "Congrats Player. you are the loser." # it would be fun to keep track of games and make snarky remarks, like you lost, again.  etc. 
-   elsif all of the board is full but no 3 in a row 
-     @message = "It is a draw."
-     #option to play again
+
     
- 
+=begin 
  def evaluate
  available_computer_moves = []
  @evaluate.attributes.each do |column_name, column_value|
@@ -200,7 +120,19 @@ end
     end
   end
   
- end
-  
+ end  
 =end
 end
+
+=begin
+def possible_wins
+  @diagonal_right = [:s0,:s4,:s8] 
+  @diagonal_left = [:s2,:s4,:s6]
+  @horizontal_top = [:s0,:s1,:s2]
+  @horizontal_middle = [:s3,:s4,:s5]
+  @horizontal_bottom = [:s6,:s7,:s8]
+  @vertical_left = [:s0,:s3,:s6]
+  @vertical_middle = [:s1,:s4,:s7]
+  @vertical_right = [:s2,:s5,:s8]
+end
+=end
